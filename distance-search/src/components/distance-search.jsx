d25ec9b5540b01b4160e89c765734670e9e11855
MohammedF425 <mohammedfadel425@gmail.com>
2023-06-18 16:58:11 -0400

    add styling, add metric options,add error handling

--- distance-search/src/components/distance-search.jsx
+++ distance-search/src/components/distance-search.jsx
@@ -1,31 +1,52 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 
+
 const DistanceSearch = () => {
     const [zipCode, setZipCode] = useState('');
     const [secondZipCode, setSecondZipCode] = useState('');
     const [distance, setDistance] = useState('');
+    const [metric, setMetric] = useState('mi');
+    const [measurement, setMeasurement] = useState('miles')
     
     const getDistance = async (e) => {
         e.preventDefault();
        
         try{
-            const res = await axios.get(`https://zip-api.eu/api/v1/distance/US-${zipCode}/US-${secondZipCode}/mi`);
+            const res = await axios.get(`https://zip-api.eu/api/v1/distance/US-${zipCode}/US-${secondZipCode}/${metric}`);
             setDistance(res.data.distance);
+            console.log(res)
+            setMeasurement('miles')
+            if(metric==='km'){
+                setMeasurement('kilometers')
+            }
         }catch(err){
             console.log(err);
+            alert("Invalid Zip Code");
         }
     }
 
     return(
-        <div>
-            <h1>Distance Finder</h1>
+        <div class="form-control form-control-lg">
+            <h1 className="main-heading">Distance Finder</h1>
             <form onSubmit={getDistance}>
                 <input type="text" placeholder="First Zipcode" value={zipCode} onChange={(e) => setZipCode(e.target.value)} />
+                <br></br>
                 <input type="text" placeholder="Second Zipcode" value={secondZipCode} onChange={(e) => setSecondZipCode(e.target.value)} />
-                <button type="submit">Find Distance</button>
+                <br></br>
+
+                <select class="" size = '2' name="metrics" multiple>
+                    <option value='mi' onClick={(e) => setMetric(e.target.value)}>miles</option>
+                    <option value='km' onClick={(e) => setMetric(e.target.value)}>kilometers</option>
+                </select> 
+                <br></br>
+     
+                <button type="submit" class="btn btn-primary">Find Distance</button>
+
+
             </form>
-            {distance && <h1>{distance} miles</h1>}
+            
+            {<h1>{distance} {measurement}</h1>}
 
         </div>
     )
