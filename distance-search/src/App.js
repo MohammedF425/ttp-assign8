10fec7027a012441c3842bc64adac1be7ceba56b
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-18 15:40:23 -0400

    Setup Distance finder using zip-api

---
+++ distance-search/src/App.js
@@ -0,0 +1,13 @@
+import DistanceSearch from './components/distance-search';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <DistanceSearch />
+    </div>
+  );
+}
+
+export default App;
